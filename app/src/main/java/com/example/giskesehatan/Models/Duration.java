ba0e6548b56d17ce1ddf402c81d425289d2b1e2a
galihsu <indosmartdg@gmail.com>
2023-10-20 16:15:32 +0700

    penambahan garis jalan pada maps

---
+++ app/src/main/java/com/example/giskesehatan/Models/Duration.java
@@ -0,0 +1,14 @@
+package com.example.giskesehatan.Models;
+
+public class Duration {
+    private String text;
+    private int value;
+
+    public String getText() {
+        return text;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ app/src/main/java/com/example/giskesehatan/Models/RoutesResponseModel.java
@@ -0,0 +1,12 @@
+package com.example.giskesehatan.Models;
+
+import java.util.List;
+
+public class RoutesResponseModel {
+    private List<Route> routes;
+
+    public List<Route> getRoutes() {
+        return routes;
+    }
+}
+
